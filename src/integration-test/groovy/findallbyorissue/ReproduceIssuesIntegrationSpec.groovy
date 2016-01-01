6fe25d39bff6c24afdf1b9314e71f2a147cd49c9
Bobby Vandiver <bobby.vandiver88@gmail.com>
2016-01-01 03:51:36 -0500

    Update tests to demonstrate problem with findByXXXOrYYY as well

--- src/integration-test/groovy/findallbyorissue/ReproduceIssuesIntegrationSpec.groovy
+++ src/integration-test/groovy/findallbyorissue/ReproduceIssuesIntegrationSpec.groovy
@@ -10,19 +10,38 @@ class ReproduceIssuesIntegrationSpec extends Specification {
 
     Author author1
     Author author2
+    Author author3
 
     Book book1
     Book book2
+    Book book3
 
     void setup() {
         author1 = new Author(name: "Stephen King").save()
         author2 = new Author(name: "Kim Stanley Robinson").save()
+        author3 = new Author(name: "Unknown").save()
 
         book1 = new Book(title: "The Gunslinger").save()
         book2 = new Book(title: "Red Mars").save()
+        book3 = new Book(title: "I Ching").save()
 
         new AuthoredBook(author: author1, book: book1).save()
         new AuthoredBook(author: author2, book: book2).save()
+    }
+
+    void "find by author or book -- dynamic finder"() {
+        expect:
+        AuthoredBook.findByAuthorOrBook(author3, book3) == null
+    }
+
+    void "find by author or book -- criteria"() {
+        expect:
+        AuthoredBook.createCriteria().get {
+            or {
+                eq("author", author3)
+                eq("book", book3)
+            }
+        } == null
     }
 
     void "find all by author or book -- dynamic finder"() {
